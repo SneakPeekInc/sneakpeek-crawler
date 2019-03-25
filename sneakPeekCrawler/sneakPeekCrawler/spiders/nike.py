42c907fd234aeef702fd3f37334770da631535f8
ownweight1 <kanamorishuhei@gmail.com>
2019-03-25 20:39:28 +0900

    ナイキ公式サイトからスニーカーの画像URLを取ってくるSpider実装_指摘修正

--- sneakPeekCrawler/sneakPeekCrawler/spiders/nike.py
+++ sneakPeekCrawler/sneakPeekCrawler/spiders/nike.py
@@ -3,14 +3,14 @@ import scrapy
 from sneakPeekCrawler.items import SneakpeekcrawlerItem
 
 
-
 class NikeSpider(scrapy.Spider):
     name = 'nike'
     allowed_domains = ['store.nike.com/jp/ja_jp/pw']
     start_urls = ['https://store.nike.com/jp/ja_jp/pw//']
 
     def parse(self, response):
-        for nike in response.css('div.exp-gridwall-content.clearfix'):
+        image_doms = response.css('div.exp-gridwall-content.clearfix')
+        for image_dom in image_doms:
             item = SneakpeekcrawlerItem()
-            item['url'] = nike.css('img::attr(src)').extract()
+            item['url'] = image_dom.css('img::attr(src)').extract()
             yield item
