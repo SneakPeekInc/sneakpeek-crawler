89ec65d6deecb3bb6ed1c03d60ae4ddbd8c6919f
SHUHEI KANAMORI <47746464+ownweight1@users.noreply.github.com>
2019-07-07 23:26:54 +0900

    Add circleci (#17)
    * XPATH修正
    
    * add:pipelines
    
    * add-pipelines
    
    * ]
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit

--- sneakPeekCrawler/pipelines.py
+++ sneakPeekCrawler/pipelines.py
@@ -5,7 +5,26 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from google.cloud import firestore
 
 class SneakpeekcrawlerPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+    def __init__(self, collection_name):
+        self.collection_name = 'sneakers'
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            collection_name=crawler.spider.name
+        )
+
+    def open_spider(self, spider):
+        self.client = firestore.Client.from_service_account_json(
+            '秘密鍵のパス')
+        self.db = self.client.collection(self.collection_name)
+
+    def process_item(self, item, spider):
+        self.db.add(dict(item))
         return item
