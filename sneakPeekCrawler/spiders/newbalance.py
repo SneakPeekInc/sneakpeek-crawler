c5cc69e89b7ed6be986049a2daa2a5c93d55acf0
ownweight1 <kanamorishuhei@gmail.com>
2019-07-08 00:00:48 +0900

    commit

---
+++ sneakPeekCrawler/spiders/newbalance.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from sneakPeekCrawler.items import Sneaker
+
+
+class NewBalanceSpider(scrapy.Spider):
+    name = 'newbalance'
+    allowed_domains = ['newbalance.jp']
+    start_urls = [
+        'https://shop.newbalance.jp/shop/goods/search.aspx?filtercode5=M&filtercode6=100&min_price=&max_price=&tree1=&keyword=']
+
+    def parse(self, response):
+
+        counter = 0  # テスト用
+
+        next_urls = response.css('div.StyleP_Item_.item')
+
+        for next_url in next_urls:
+            url = next_url.css('a::attr(href)').extract_first()
+            yield scrapy.Request(response.urljoin(url), self.parse_items)
+
+            counter += 1  # テスト用
+
+            if counter == 5:  # テスト用
+                return  # テスト用
+
+    def parse_items(self, response):
+
+        NAME_XPATH = '//div[@class="main"]/h3/text()'
+        PRICE_XPATH = '//div[@class="main"]/p[@class="price"]/span/text()'
+        IMAGE_URLS_XPATH = "//a[@class='active']/img/@src"
+
+        def string_price_format_number(price):
+            price = price.translate(str.maketrans(
+                {',': None, '円': None, '＋': None, '(': None, '税': None, ')': None}))
+            price = int(price)
+
+            return price
+
+        def image_url_format(url):
+            url = response.urljoin(url)
+            url = url.replace('L_T', 'L_O')
+
+            return url
+
+        item = Sneaker()
+        item['name'] = response.xpath(NAME_XPATH).extract_first()
+        item['price'] = string_price_format_number(
+            response.xpath(PRICE_XPATH).extract_first())
+        item['image_urls'] = image_url_format(
+            response.xpath(IMAGE_URLS_XPATH).extract_first())
+
+        yield item
