3f09260a96c74761c8c9f29d91d506f625306088
SHUHEI KANAMORI <47746464+ownweight1@users.noreply.github.com>
2019-07-17 08:08:31 +0900

    fix:開発時はドキュメントを削除 (#19)
    * 削除
    
    * nikeのURL修正

--- sneakPeekCrawler/spiders/newbalance.py
+++ sneakPeekCrawler/spiders/newbalance.py
@@ -16,7 +16,7 @@ class NewBalanceSpider(scrapy.Spider):
             url = next_url.css('a::attr(href)').extract_first()
             yield scrapy.Request(response.urljoin(url), self.parse_items)
 
-            if utils.isDevelopment() and n > 3: return
+            if utils.isDevelopment() and n > 1: return
 
     def parse_items(self, response):
 
