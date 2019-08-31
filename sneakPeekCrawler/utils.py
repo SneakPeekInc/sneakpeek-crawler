addc8ed6eb0e33e129344331a7091affc78d3558
ownweight1 <kanamorishuhei@gmail.com>
2019-08-31 16:46:00 +0900

    bugfix

--- sneakPeekCrawler/utils.py
+++ sneakPeekCrawler/utils.py
@@ -7,8 +7,5 @@ def get_brands():
     return ['nike', 'newbalance']
 
 def crawling(brand):
-    os.chdir("sneakPeekCrawler")
     os.system("scrapy crawl " + brand)
-    os.chdir("..")
     return "done"
-
